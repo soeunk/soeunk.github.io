59485ddf3441fb3e6053c8186f9baf24a26902c1
soeunk <88478692+soeunk@users.noreply.github.com>
2021-08-19 15:46:02 +0900

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,40 @@
+var coverImage=document.querySelector('#cover1');
+         coverImage.onclick=function(){
+           alert('눌렀습니다.');
+           
+         }
+         coverImage.onmouseover=function(){
+           coverImage.style.border="4px red solid";
+          
+         }
+         coverImage.onmouseout=function(){
+           coverImage.style.border=null;
+         }
+
+var coverImage2=document.querySelector('#cover2');
+         coverImage2.onclick=function(){
+           alert('눌렀습니다.');
+           
+         }
+         coverImage2.onmouseover=function(){
+           coverImage2.style.border="4px red solid";
+          
+         }
+         coverImage2.onmouseout=function(){
+           coverImage2.style.border=null;
+         }
+
+var coverImage3=document.querySelector('#cover3');
+         coverImage3.onclick=function(){
+           alert('눌렀습니다.');
+           
+         }
+         coverImage3.onmouseover=function(){
+           coverImage3.style.border="4px red solid";
+          
+         }
+         coverImage3.onmouseout=function(){
+           coverImage3.style.border=null;
+         }         
+     
+	
