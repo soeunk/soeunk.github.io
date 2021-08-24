9ff154d484ece727b1f74e86e82d520e579e5d8e
soeunk <88478692+soeunk@users.noreply.github.com>
2021-08-25 04:16:43 +0900

    Delete hehe.js

--- yeah/hehe.js
+++
@@ -1,8 +0,0 @@
-new fullpage('#full-page', { 
-        licenseKey:'',
-        sectionsColor: ['#ff0000', '#00ff00', '#0000ff', '#aaaaaa'],
-        navigation: true,
-        navigationTooltips: ['Fisrst page', 'Second page', 'Third and the list page']
-        scrollingSpeed:20000
-    });
-
