7e7891f88e3bf7e3724572e87eaccebba69ad8f6
soeunk <88478692+soeunk@users.noreply.github.com>
2021-08-25 00:51:38 +0900

    Add files via upload

---
+++ hoo/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+  // fullpage customization
+  $('#fullpage').fullpage({
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    css3: true,
+    controlArrows: false    
+  }); 
+  
+});
