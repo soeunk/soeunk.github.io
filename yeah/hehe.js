87033b8c68f7403548a807dae1db12e383293f5e
soeunk <88478692+soeunk@users.noreply.github.com>
2021-08-25 03:54:56 +0900

    Create hehe.js

---
+++ yeah/hehe.js
@@ -0,0 +1,6 @@
+new fullpage('#full-page', {
+        licenseKey: '',
+        sectionsColor: ['#ff0000', '#00ff00', '#0000ff', '#aaaaaa'],
+        navigation: true,
+        navigationTooltips: ['Fisrst page', 'Second page', 'Third and the list page']
+    });
