c19fa7e4015d98d03620fa2251e288e8b8569a90
soeunk <88478692+soeunk@users.noreply.github.com>
2021-08-25 03:14:17 +0900

    Create test.js

---
+++ yeah/test.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+		$(document).ready(function() {
+			$('#fullpage').fullpage({
+				anchors: ['firstPage', 'secondPage', '3rdPage'],
+				sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+				navigation: true,
+				navigationPosition: 'right',
+				navigationTooltips: ['First page', 'Second page', 'Third and last page']
+			});
+		});
+	</script>
