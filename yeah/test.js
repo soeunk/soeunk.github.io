a80c29130dd2a40d7e88c8137eaf9bf1f468ad9b
soeunk <88478692+soeunk@users.noreply.github.com>
2021-08-26 01:38:16 +0900

    Update test.js

--- yeah/test.js
+++ yeah/test.js
@@ -1,11 +1,9 @@
-<script type="text/javascript">
-		$(document).ready(function() {
-			$('#fullpage').fullpage({
-				anchors: ['firstPage', 'secondPage', '3rdPage'],
-				sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-				navigation: true,
-				navigationPosition: 'right',
-				navigationTooltips: ['First page', 'Second page', 'Third and last page']
-			});
-		});
-	</script>
+var coverImage=document.querySelector('#but');
+
+         coverImage.onmouseover=function(){
+           coverImage.style.border="4px red solid";
+          
+         }
+         coverImage.onmouseout=function(){
+           coverImage.style.border=null;
+         }
